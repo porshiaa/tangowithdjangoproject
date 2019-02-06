56537c4241a0d409d3e7e4370ac47240acc218b5
porshiaa <46494094+porshiaa@users.noreply.github.com>
2019-02-06 15:15:06 +0000

    Add files via upload

---
+++ tango_with_django_project/tango_with_django_project/rango/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from rango.models import Category, Page
+
+
+class PageAdmin (admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    def __str__(self):
+        return self.list_display
+
+
+admin.site.register(Page, PageAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+
