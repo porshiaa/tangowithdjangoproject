7e5eeeb3575ce9ee7ef56c73dd7aaabedc85957d
porshiaa <46494094+porshiaa@users.noreply.github.com>
2019-02-09 16:20:02 +0000

    Add files via upload

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from rango.models import Page, Category
-
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -19,8 +19,33 @@ class PageForm(forms.ModelForm):
                             help_text="Please enter the title of the page.")
     url = forms.CharField(max_length=200,
                           help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), intial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
